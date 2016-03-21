3c6b1e7f043a4a516c51a0c573b6015555ade713
Azarias <azarias.boutin@gmail.com>
2016-03-21 17:01:05 +0000

    Cleaned up javascript code

--- web/assets/js/editProject.js
+++ web/assets/js/editProject.js
@@ -2,95 +2,83 @@
 
 $(document).ready(function () {
 
+    var actions = {
+        'add-meeting': {
+            'formId': 'create-meeting',
+            'listId': 'list-meetings',
+            'newLine': function (data) {
+                var meeting = data.meeting;
+                var date = new Date(meeting.date.date);
+                return $('<li class="list-group-item" data-id="' + meeting.id + '" >' +
+                        ' Meeting at ' + this.dateString(date) +
+                        ' in room  ' + meeting.room +
+                        '<span class="pull-right">' +
+                        '<a href="#" class="btn btn-primary btn-xs">' +
+                        '<i class="glyphicon glyphicon-pencil"></i> ' +
+                        ' Edit </a> <a href="#" class="btn btn-warning btn-xs"> ' +
+                        '<i class="glyphicon glyphicon-remove"></i> ' +
+                        ' Delete </a></span></li>');
+            },
+            'dateString': function (date) {
+                return date.getDate() + '/' + date.getMonth() + '/' +
+                        date.getFullYear() + ' ' + date.getHours() + 1 + ':' + date.getMinutes();
+            }
+        },
+        'add-role': {
+            'formId': 'create-role',
+            'listId': 'list-roles',
+            'newLine': function (data) {
+                var role = data.role;
+                return  $('<li class="list-group-item" data-id="{{ ro.id }}" >' +
+                        '<strong>' + role.student.name + '</strong> : ' +
+                        role.name + '<span class="pull-right">' +
+                        '<a href="#" class="btn btn-primary btn-xs">' +
+                        '<i class="glyphicon glyphicon-pencil"></i> ' +
+                        ' Edit </a> \n <a href="#" class="btn btn-warning btn-xs">' +
+                        '<i class="glyphicon glyphicon-remove"></i> ' +
+                        ' Delete </a></span></li>');
+            }
+        }
+    };
+
     function init() {
-        $("#button-add-meeting").on('click', launchMeetingModal);
-        $("#button-add-role").on('click', launchRoleModal);
-    }
-
-    function launchRoleModal(e) {
-        e.preventDefault();
-        var url = $(this).attr('href');
-        var pId = $(this).data('project');
-        $.post(url, {'project-id': pId}, function (res) {
-            $("#modal-role-content").html(res);
-            $("#form-create-role").on('submit', function (e) {
+        Object.keys(actions).forEach(function (i) {
+            var id = "button-" + i;
+            $("#" + id).on('click', function (e) {
                 e.preventDefault();
-                $(this).find("#project-id").val(pId);
-                formSubmitted(url, this, respRole);
+                var url = $(this).attr('href');
+                var pId = $(this).data('project');
+                firstRequest(url, pId, actions[i]);
             });
-            $("#modal-role").modal();
         });
     }
 
-    function launchMeetingModal(e) {
-        e.preventDefault();
-        var url = $(this).attr('href');
-        var pId = $(this).data('project');
-        $.post(url, {}, function (res) {
-            $("#modal-meeting-content").html(res);
-            $("#form-create-meeting").on('submit', function (e) {
+    function firstRequest(url, projectId, obj) {
+        $.post(url, {'project-id': projectId}, function (res) {
+            $("#modal-main-content").html(res);
+            $("#form-" + obj.formId).on('submit', function (e) {
                 e.preventDefault();
-                $(this).find('#project-id').val(pId);
-                formSubmitted(url, this, respMeeting);
+                $(this).find("#project-id").val(projectId);
+                formSubmitted(url, this, obj);
             });
-            $("#modal-meeting").modal();
+            $("#modal-main").modal();
         });
     }
 
-    function respRole(data) {
-        if (data.success) {
-            var role = data.role;
-            createRoleLine(role);
-            $("#modal-role").modal('hide');
-        } else {
-            $("#modal-role-content").html(data.page);
-        }
+    function addLineTo(listId, newLine) {
+        $("#" + listId).append(newLine);
     }
 
-    function formSubmitted(url, form, callBack) {
+    function formSubmitted(url, form, obj) {
         var $form = $(form);
-        $.post(url, $form.serialize(), callBack);
-    }
-
-    function respMeeting(data) {
-        if (data.success) {
-            var meeting = data.meeting;
-            createMeetingLine(meeting);
-            $("#modal-meeting").modal('hide');
-        } else {
-            $("#modal-meeting-content").html(data.page);
-        }
-    }
-
-    function createRoleLine(role) {
-        var $li = $('<li class="list-group-item" data-id="{{ ro.id }}" >' +
-                '<strong>' + role.student.name + '</strong> : ' +
-                role.name + '<span class="pull-right">' +
-                '<a href="#" class="btn btn-primary btn-xs">' +
-                '<i class="glyphicon glyphicon-pencil"></i> ' +
-                ' Edit </a> \n <a href="#" class="btn btn-warning btn-xs">' +
-                '<i class="glyphicon glyphicon-remove"></i> ' +
-                ' Delete </a></span></li>');
-        $("#list-roles").append($li);
-    }
-
-    function createMeetingLine(meeting) {
-        var date = new Date(meeting.date.date);
-        var $li = $('<li class="list-group-item" data-id="' + meeting.id + '" >' +
-                ' Meeting at ' + dateString(date) +
-                ' in room  ' + meeting.room +
-                '<span class="pull-right">' +
-                '<a href="#" class="btn btn-primary btn-xs">' +
-                '<i class="glyphicon glyphicon-pencil"></i> ' +
-                ' Edit </a> <a href="#" class="btn btn-warning btn-xs"> ' +
-                '<i class="glyphicon glyphicon-remove"></i> ' +
-                ' Delete </a></span></li>');
-        $("#list-meetings").append($li);
-    }
-
-    function dateString(date) {
-        return date.getDate() + '/' + date.getMonth() + '/' +
-                date.getFullYear() + ' ' + date.getHours() + 1 + ':' + date.getMinutes();
+        $.post(url, $form.serialize(), function (data) {
+            if (data.success) {
+                addLineTo(obj.listId, obj.newLine.call(obj, data));
+                $("#modal-main").modal('hide');
+            } else {
+                $("#modal-main-content").html(data.page);
+            }
+        });
     }
 
     init();
